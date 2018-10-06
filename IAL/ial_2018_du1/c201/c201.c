2f9b7aa0e7e19dc129e76d2f2190e4da78bf8031
IamSerggo <xbabic09@stud.fit.vutbr.cz>
2018-10-06 16:56:05 +0200

    IAl project

---
+++ IAL/ial_2018_du1/c201/c201.c
@@ -0,0 +1,242 @@
+
+/* c201.c *********************************************************************}
+{* Téma: Jednosměrný lineární seznam
+**
+**                     Návrh a referenční implementace: Petr Přikryl, říjen 1994
+**                                          Úpravy: Andrea Němcová listopad 1996
+**                                                   Petr Přikryl, listopad 1997
+**                                Přepracované zadání: Petr Přikryl, březen 1998
+**                                  Přepis do jazyka C: Martin Tuček, říjen 2004
+**                                            Úpravy: Kamil Jeřábek, září 2018
+**
+** Implementujte abstraktní datový typ jednosměrný lineární seznam.
+** Užitečným obsahem prvku seznamu je celé číslo typu int.
+** Seznam bude jako datová abstrakce reprezentován proměnnou typu tList.
+** Definici konstant a typů naleznete v hlavičkovém souboru c201.h.
+**
+** Vaším úkolem je implementovat následující operace, které spolu s výše
+** uvedenou datovou částí abstrakce tvoří abstraktní datový typ tList:
+**
+**      InitList ...... inicializace seznamu před prvním použitím,
+**      DisposeList ... zrušení všech prvků seznamu,
+**      InsertFirst ... vložení prvku na začátek seznamu,
+**      First ......... nastavení aktivity na první prvek,
+**      CopyFirst ..... vrací hodnotu prvního prvku,
+**      DeleteFirst ... zruší první prvek seznamu,
+**      PostDelete .... ruší prvek za aktivním prvkem,
+**      PostInsert .... vloží nový prvek za aktivní prvek seznamu,
+**      Copy .......... vrací hodnotu aktivního prvku,
+**      Actualize ..... přepíše obsah aktivního prvku novou hodnotou,
+**      Succ .......... posune aktivitu na další prvek seznamu,
+**      Active ........ zjišťuje aktivitu seznamu.
+**
+** Při implementaci funkcí nevolejte žádnou z funkcí implementovaných v rámci
+** tohoto příkladu, není-li u dané funkce explicitně uvedeno něco jiného.
+**
+** Nemusíte ošetřovat situaci, kdy místo legálního ukazatele na seznam předá
+** někdo jako parametr hodnotu NULL.
+**
+** Svou implementaci vhodně komentujte!
+**
+** Terminologická poznámka: Jazyk C nepoužívá pojem procedura.
+** Proto zde používáme pojem funkce i pro operace, které by byly
+** v algoritmickém jazyce Pascalovského typu implemenovány jako
+** procedury (v jazyce C procedurám odpovídají funkce vracející typ void).
+**/
+
+#include "c201.h"
+
+int errflg;
+int solved;
+
+void Error() {
+/*
+** Vytiskne upozornění na to, že došlo k chybě.
+** Tato funkce bude volána z některých dále implementovaných operací.
+**/
+    printf ("*ERROR* The program has performed an illegal operation.\n");
+    errflg = TRUE;                      /* globální proměnná -- příznak chyby */
+}
+
+void InitList (tList *L) {
+/*
+** Provede inicializaci seznamu L před jeho prvním použitím (tzn. žádná
+** z následujících funkcí nebude volána nad neinicializovaným seznamem).
+** Tato inicializace se nikdy nebude provádět nad již inicializovaným
+** seznamem, a proto tuto možnost neošetřujte. Vždy předpokládejte,
+** že neinicializované proměnné mají nedefinovanou hodnotu.
+**/
+
+  L->First = NULL; //prvy prvok na NULL
+  L->Act = NULL; //aktivny prvok na NULL
+
+// solved = FALSE;                   /* V případě řešení, smažte tento řádek! */
+}
+
+void DisposeList (tList *L) {
+/*
+** Zruší všechny prvky seznamu L a uvede seznam L do stavu, v jakém se nacházel
+** po inicializaci. Veškerá paměť používaná prvky seznamu L bude korektně
+** uvolněna voláním operace free.
+***/
+
+  tElemPtr tmp; //pomocna premenna
+
+  L->Act = NULL; //zrusim aktivny prvok
+
+  while (L->First != NULL){ //pokracujem az kym nezrusim vsetky prvky
+    tmp = L->First; //prvy si dam do premennej aby som ju mohol uvolnit
+    L->First = tmp->ptr; //posuniem ukazovatel
+    free(tmp); //zrusim prvok
+  }
+  //solved = FALSE;                   /* V případě řešení, smažte tento řádek! */
+}
+
+void InsertFirst (tList *L, int val) {
+/*
+** Vloží prvek s hodnotou val na začátek seznamu L.
+** V případě, že není dostatek paměti pro nový prvek při operaci malloc,
+** volá funkci Error().
+**/
+
+  tElemPtr tmp = malloc(sizeof(struct tElem));
+  if (tmp == NULL) Error(); //ked sa alokacia nepodari, volam funkciu Error
+
+  else{
+    tmp->data = val; //do pomocneho prvku si dam data parametru val
+    tmp->ptr = L->First; //posuniem ukazovatel
+    L->First = tmp; //na prve miesto dam tmp prvok s hodnotou val
+  }
+  //solved = FALSE;                   /* V případě řešení, smažte tento řádek! */
+}
+
+void First (tList *L) {
+/*
+** Nastaví aktivitu seznamu L na jeho první prvek.
+** Funkci implementujte jako jediný příkaz, aniž byste testovali,
+** zda je seznam L prázdný.
+**/
+
+  L->Act = L->First; //nastavim prvy prvok ako aktivny
+  //solved = FALSE;                   /* V případě řešení, smažte tento řádek! */
+}
+
+void CopyFirst (tList *L, int *val) {
+/*
+** Prostřednictvím parametru val vrátí hodnotu prvního prvku seznamu L.
+** Pokud je seznam L prázdný, volá funkci Error().
+**/
+
+  if (L->First == NULL) Error();//kontrola prazdneho zoznamu
+
+  else{
+    *val = L->First->data; //ak nie je prazdny, dam hodnotu prveho do val
+  }
+//  solved = FALSE;                   /* V případě řešení, smažte tento řádek! */
+}
+
+void DeleteFirst (tList *L) {
+/*
+** Zruší první prvek seznamu L a uvolní jím používanou paměť.
+** Pokud byl rušený prvek aktivní, aktivita seznamu se ztrácí.
+** Pokud byl seznam L prázdný, nic se neděje.
+**/
+
+  if (L->First != NULL){
+    if (L->First == L->Act) L->Act = NULL; //ak je prvy aktivny, rusim aktivitu
+
+    tElemPtr tmp;
+    tmp = L->First;
+    L->First = tmp->ptr;
+    free(tmp); //prvy prvok dam do pomocneho prvku, posuniem ukazovatel a prvy zrusim
+
+  }
+  //  solved = FALSE;                   /* V případě řešení, smažte tento řádek! */
+}
+
+void PostDelete (tList *L) {
+/*
+** Zruší prvek seznamu L za aktivním prvkem a uvolní jím používanou paměť.
+** Pokud není seznam L aktivní nebo pokud je aktivní poslední prvek seznamu L,
+** nic se neděje.
+**/
+
+  if ((L->Act != NULL) && (L->Act->ptr != NULL)){
+    tElemPtr tmp;
+    tmp = L->Act->ptr;
+    L->Act->ptr = tmp->ptr;
+    free(tmp);
+  }
+ //solved = FALSE;                   /* V případě řešení, smažte tento řádek! */
+}
+
+void PostInsert (tList *L, int val) {
+/*
+** Vloží prvek s hodnotou val za aktivní prvek seznamu L.
+** Pokud nebyl seznam L aktivní, nic se neděje!
+** V případě, že není dostatek paměti pro nový prvek při operaci malloc,
+** zavolá funkci Error().
+**/
+
+  if (L->Act != NULL){
+    tElemPtr tmp;
+    tmp = malloc(sizeof(struct tElem));
+    if (tmp == NULL) Error();
+
+    else{
+      tmp->data = val;
+      tmp->ptr = L->Act->ptr;
+      L->Act->ptr = tmp;
+    }
+  }
+ //solved = FALSE;                   /* V případě řešení, smažte tento řádek! */
+}
+
+void Copy (tList *L, int *val) {
+/*
+** Prostřednictvím parametru val vrátí hodnotu aktivního prvku seznamu L.
+** Pokud seznam není aktivní, zavolá funkci Error().
+**/
+
+  if (L->Act == NULL) Error();
+  else *val = L->Act->data;
+
+// solved = FALSE;                   /* V případě řešení, smažte tento řádek! */
+}
+
+void Actualize (tList *L, int val) {
+/*
+** Přepíše data aktivního prvku seznamu L hodnotou val.
+** Pokud seznam L není aktivní, nedělá nic!
+**/
+
+  if (L->Act != NULL) L->Act->data = val;
+
+ //solved = FALSE;                   /* V případě řešení, smažte tento řádek! */
+}
+
+void Succ (tList *L) {
+/*
+** Posune aktivitu na následující prvek seznamu L.
+** Všimněte si, že touto operací se může aktivní seznam stát neaktivním.
+** Pokud není předaný seznam L aktivní, nedělá funkce nic.
+**/
+
+  if (L->Act != NULL){
+    if (L->Act->ptr != NULL) L->Act = L->Act->ptr;
+    else L->Act = NULL;
+  }
+ //solved = FALSE;                   /* V případě řešení, smažte tento řádek! */
+}
+
+int Active (tList *L) {
+/*
+** Je-li seznam L aktivní, vrací nenulovou hodnotu, jinak vrací 0.
+** Tuto funkci je vhodné implementovat jedním příkazem return.
+**/
+
+  return (L->Act != NULL ? 1 : 0);
+ //solved = FALSE;                   /* V případě řešení, smažte tento řádek! */
+}
+
+/* Konec c201.c */
